5e7ddd79f88afdb8bcb55d60098a02847b06be8e
J-ZW-Wang <zhiweiwang@Gmail.com>
2024-10-02 22:50:46 -0500

    Inital solution

---
+++ Rectangles/main.py
@@ -0,0 +1,55 @@
+import re
+
+
+def rectangles(strings):
+    rec_counter = 0
+    for i, line in enumerate(strings):
+        print(f"Examining line [{i}]: {line}")
+        while re.search(r"(\+-*\+)", line) != None:
+            top_side = re.search(r"(\+-*\+)", line)  # Find patten '+-*-+'
+            while top_side != None:  # Loop through all top sides in the 'line'
+                s = top_side.start()
+                e = top_side.end() - 1
+                next_line_index = i + 1
+                ndashes = e - s - 1
+                while next_line_index < len(
+                    strings
+                ):  # loop through the verticle pattens
+                    next_line = strings[next_line_index]
+                    if (
+                        next_line[s] in "|+" and next_line[e] in "|"
+                    ):  # regular sidelines
+                        next_line_index += 1
+                    elif next_line[s] in "|" and next_line[e] in "|+":
+                        next_line_index += 1
+                    elif (
+                        re.match(rf"\+[-\+]{{{ndashes}}}\+", next_line[s : e + 1])
+                        != None
+                    ):  # the lower side must be in the pattern of +-..--+
+                        rec_counter += 1
+                        print(
+                            f"Boxes = {rec_counter}, topside = {top_side.group(0)}, line_index = {next_line_index} {next_line}"
+                        )
+                        next_line_index += 1
+                    else:  # no box found
+                        next_line_index = len(strings) + 1  # break the loop
+                current_match_re = re.sub("\\+", "\+", top_side.group(0))
+                next_match = f"{current_match_re}-*\\+"
+                top_side = re.search(next_match, line)
+            line = line[:s] + "-" + line[s + 1 :]  # Replace the start of topside as '-'
+            print(f"current line {line}")
+    return rec_counter
+
+
+test_string = [
+    "+-+ +-+",
+    "| | | |",
+    "+-+-+-+",
+    "  | |  ",
+    "+-+-+-+",
+    "| | | |",
+    "+-+ +-+",
+]
+
+
+print(rectangles(test_string))
