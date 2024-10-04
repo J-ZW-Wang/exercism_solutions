b5120fe446874c87755cf5bf807fcdde5971e2d6
J-ZW-Wang <zhiweiwang@Gmail.com>
2024-10-03 21:27:50 -0500

    minor format change

--- Rectangles/main.py
+++ Rectangles/main.py
@@ -4,7 +4,6 @@ import re
 def rectangles(strings):
     rec_counter = 0
     for i, line in enumerate(strings):
-        print(f"Examining line [{i}]: {line}")
         while re.search(r"(\+-*\+)", line) != None:
             top_side = re.search(r"(\+-*\+)", line)  # Find patten '+-*-+'
             while top_side != None:  # Loop through all top sides in the 'line'
@@ -13,31 +12,25 @@ def rectangles(strings):
                 next_line_index = i + 1
                 ndashes = e - s - 1
                 while next_line_index < len(
-                    strings
-                ):  # loop through the verticle pattens
+                        strings):  # loop through the verticle pattens
                     next_line = strings[next_line_index]
-                    if (
-                        next_line[s] in "|+" and next_line[e] in "|"
-                    ):  # regular sidelines
+                    if (next_line[s] in "|+"
+                            and next_line[e] in "|"):  # regular sidelines
                         next_line_index += 1
                     elif next_line[s] in "|" and next_line[e] in "|+":
                         next_line_index += 1
-                    elif (
-                        re.match(rf"\+[-\+]{{{ndashes}}}\+", next_line[s : e + 1])
-                        != None
-                    ):  # the lower side must be in the pattern of +-..--+
+                    elif (re.match(rf"\+[-\+]{{{ndashes}}}\+",
+                                   next_line[s:e + 1]) != None):
+                        # the lower side must be in the pattern of +-..--+
                         rec_counter += 1
-                        print(
-                            f"Boxes = {rec_counter}, topside = {top_side.group(0)}, line_index = {next_line_index} {next_line}"
-                        )
                         next_line_index += 1
                     else:  # no box found
                         next_line_index = len(strings) + 1  # break the loop
                 current_match_re = re.sub("\\+", "\+", top_side.group(0))
                 next_match = f"{current_match_re}-*\\+"
                 top_side = re.search(next_match, line)
-            line = line[:s] + "-" + line[s + 1 :]  # Replace the start of topside as '-'
-            print(f"current line {line}")
+            line = line[:s] + "-" + line[
+                s + 1:]  # Replace the start of topside as '-'
     return rec_counter
 
 
@@ -45,11 +38,10 @@ test_string = [
     "+-+ +-+",
     "| | | |",
     "+-+-+-+",
-    "  | |  ",
+    "  | | |",
     "+-+-+-+",
     "| | | |",
     "+-+ +-+",
 ]
-
 
 print(rectangles(test_string))
